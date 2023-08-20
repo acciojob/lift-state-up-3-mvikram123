f015aa98793fe33ca7e2d1e28c409f630441066c
manbodhvikram <manbodhvikram@gmail.com>
2023-08-20 16:56:19 +0530

    submitting code-Sun Aug 20 2023 16:56:15 GMT+0530 (India Standard Time)

--- src/components/Child.js
+++ src/components/Child.js
@@ -24,10 +24,10 @@ const Child=({selectedOption,setSelectedOption})=>{
         <div className="child">
 
             <div className="child1">
-            <button onClick={click1}>option1</button>
+            <button onClick={click1}>Option 1</button>
             </div>
             <div className="child2">
-            <button onClick={click2}>option2</button>
+            <button onClick={click2}>Option 2</button>
                 
             </div>
 
